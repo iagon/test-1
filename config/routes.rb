2c46139d4ad9619c83e02484bc7531fb94486550
iagon <iangonnella@gmail.com>
2017-05-28 20:39:49 -0700

    Add posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Blog::Application.routes.draw do
+    resources :posts
+    root "posts#index"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +72,4 @@ Blog::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
